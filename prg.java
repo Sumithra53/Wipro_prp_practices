739bb1c22566b998433882b1cbaa4fb253d9a664
SUMITHRA <51500842+Sumithra53@users.noreply.github.com>
2019-06-08 11:06:51 +0530

    Create prg.java

---
+++ prg.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;  
+public class Prg {  
+   public static void main(String[] args)   
+   {  
+       int  temp, sum=0, n;  
+       Scanner sc = new Scanner(System.in);  
+       n=sc.nextInt();  
+       temp=n;  
+       while(n>0)  
+       {  
+           int r = n%10;  
+           sum = sum*10+r;  
+           n = n/10;  
+       }  
+       System.out.println("Reverse of Number is:-" +sum);  
+       if(temp==sum)  
+       {  
+           System.out.println("Palindrom");  
+       }  
+       else  
+       {  
+           System.out.println("Not Palindrom");  
+       }  
+   }  
+}  
